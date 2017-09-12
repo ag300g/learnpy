f53893bd5b254444b2bc76cd1ac6b0051194e952
ag300g <ag300g@163.com>
2017-09-12 15:49:53 +0800

    Update lowess.py

--- scripts/lowess.py
+++ scripts/lowess.py
@@ -6,6 +6,17 @@ y = np.sin(x) + np.random.normal(size=len(x))
 z = lowess(y, x)  ## 默认情况下，frac=0.6666666 it=3， delta=0
 w = lowess(y, x, frac=1./3)
 
+
+
+
+import scipy.stats as stats
+x = np.random.uniform(low = -2*np.pi, high = 2*np.pi, size=500)
+y = np.sin(x) + stats.cauchy.rvs(size=len(x))
+z = lowess(y, x, frac= 1./3, it=0)
+w = lowess(y, x, frac=1./3)
+
+
+
 '''
 http://www.statsmodels.org/stable/generated/statsmodels.nonparametric.smoothers_lowess.lowess.html#statsmodels.nonparametric.smoothers_lowess.lowess
 '''
