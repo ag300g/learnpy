5e4a3cc3e99ee8f97b0f9efc1b2691fcdd4eb476
ag300g <ag300g@163.com>
2017-09-12 15:42:42 +0800

    Update lowess.py

--- scripts/lowess.py
+++ scripts/lowess.py
@@ -3,5 +3,9 @@ import statsmodels.api as sm
 lowess = sm.nonparametric.lowess
 x = np.random.uniform(low = -2*np.pi, high = 2*np.pi, size=500)
 y = np.sin(x) + np.random.normal(size=len(x))
-z = lowess(y, x)
+z = lowess(y, x)  ## 默认情况下，frac=0.6666666 it=3， delta=0
 w = lowess(y, x, frac=1./3)
+
+'''
+http://www.statsmodels.org/stable/generated/statsmodels.nonparametric.smoothers_lowess.lowess.html#statsmodels.nonparametric.smoothers_lowess.lowess
+'''
